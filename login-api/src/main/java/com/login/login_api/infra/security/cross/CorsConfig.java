7fdbde91e9a783f147f00c51aaad94073c9a7ae9
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-03-20 23:52:41 -0300

    Classe de configuração Cross

---
+++ login-api/src/main/java/com/login/login_api/infra/security/cross/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.login.login_api.infra.security.cross;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
